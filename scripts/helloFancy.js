c4a82081040304bb03b2eb54d95893dd6bf66a9c
rizkypur <rizky.dharmawan@aruna.id>
2023-10-02 20:00:09 +0700

    Add Contributors and Script

---
+++ scripts/helloFancy.js
@@ -0,0 +1,8 @@
+
+console.log("  _   _      _ _  ");
+console.log(" | | | |    | | | ");
+console.log(" | |_| | ___| | | ___");
+console.log(" |  _  |/ _ \\ | |/ _ \\");
+console.log(" | | | |  __/ | | (_) |");
+console.log(" |_| |_|\\___|_|_|\\___/");
+  
